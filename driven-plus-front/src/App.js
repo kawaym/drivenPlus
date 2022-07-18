d8fad7e7cec721c3aa1a29d4210005c7906a2e4c
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2022-07-18 16:47:16 -0300

    initial commit

---
+++ driven-plus-front/src/App.js
@@ -0,0 +1,12 @@
+import ResetStyle from "./styles/reset";
+
+function App() {
+  return (
+    <>
+      <ResetStyle />
+      <div className="App">Driven Plus</div>
+    </>
+  );
+}
+
+export default App;
